1b4f4bda81d5e7d1c5dcfe8d48632db40be7d0ed
Rizkyaditiya21 <rizkyaditiyafernando@gmail.com>
2026-02-16 11:32:49 +0700

    Update grid.js

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,1 +1,44 @@
 
+const rows = 4;
+const cols = 5;
+
+const symbols = ["🀄","🀅","🀆","🀇","🀈","🀉","🀊"];
+
+let gridData = [];
+
+function randomSymbol(){
+    return symbols[Math.floor(Math.random()*symbols.length)];
+}
+
+function createGrid(){
+
+    gridData = [];
+
+    for(let r=0; r<rows; r++){
+
+        let row = [];
+
+        for(let c=0; c<cols; c++){
+            row.push(randomSymbol());
+        }
+
+        gridData.push(row);
+    }
+
+    renderGrid();
+}
+
+function renderGrid(){
+
+    const grid = document.getElementById("grid");
+    grid.innerHTML = "";
+
+    gridData.forEach(row=>{
+        row.forEach(symbol=>{
+            let tile = document.createElement("div");
+            tile.classList.add("tile");
+            tile.innerText = symbol;
+            grid.appendChild(tile);
+        });
+    });
+}
